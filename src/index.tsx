ad399ab8355074f9ec920a502f9fccf5d1cb9a0d
szynkie <szynekq@gmail.com>
2021-04-07 20:08:47 +0200

    dupa

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+{
+  id:1,
+  img: 'https://ecsmedia.pl/c/rydzyk-i-przyjaciele-krete-sciezki-p-iext69596477.jpg',
+  title: 'ksionszka',
+  author: 'Krzychu'
+},
+
+{
+  id:2,
+  img: 'https://ecsmedia.pl/c/afekt-joanna-chylka-tom-13-p-iext69458757.jpg',
+  title: 'ksienga',
+  author: 'Zbychu'
+},
+{
+  id:3,
+  img: 'https://ecsmedia.pl/c/terapeutka-p-iext69597247.jpg',
+  title: 'ksiunszka',
+  author: 'Maciek'
+}
+]
+
+function BookList(){
+  return (
+  <section className="booklist">
+   {books.map((book)=>{
+     return <Book key={book.id} {...book}></Book> 
+   })}
+  </section>
+  )
+}
+
+const Book = ({img, title, author}:any) =>{
+  
+  return (
+  <article className="book">
+    <img src={img} alt=""/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+  </article>
+  )
+}
+
+ReactDom.render(<BookList/>,document.getElementById('root'))
