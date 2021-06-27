90431c0457a1c602c1bfd97f8fccc91845708922
szynek <szynekq@gmail.com>
2021-06-27 18:48:26 +0200

    Updates

--- src/store.tsx
+++
@@ -1,6 +0,0 @@
-import {NotificationReducer} from './reducers/NotificationReducer';
-import {createStore} from 'redux';
-
-const storeCreator = createStore as any;
-
-export const store = storeCreator(NotificationReducer);
